f1751dc90dab79ac2d5efe4c4e40691d02a5a9af
parkUiery <parkuiery@gmail.com>
2023-06-04 23:18:51 +0900

    파일 수정

--- JavaStudy/src/StudentExamole.java
+++
@@ -1,9 +0,0 @@
-public class StudentExamole {
-    public static void main(String[] args) {
-        Student s1 = new Student();
-        System.out.println("s1 변수가 Student 객체를 참조합니다");
-
-        Student s2 = new Studnet();
-        System.out.println("s2 변수가 또 다른 Studnet 객체를 참조합니다.");
-    }
-}
