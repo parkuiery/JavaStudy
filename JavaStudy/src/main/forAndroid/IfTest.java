be4613f6656d302d07f5e07c01c52a4d5e64dd91
parkUiery <parkuiery@gmail.com>
2023-06-05 23:20:33 +0900

    자바 문제 풀기

---
+++ JavaStudy/src/main/forAndroid/IfTest.java
@@ -0,0 +1,11 @@
+package PACKAGE_NAME.main.forAndroid;
+import java.util.Scanner;
+
+public class IfTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(a+b);
+    }
+}
