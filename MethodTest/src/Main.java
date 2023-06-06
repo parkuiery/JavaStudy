8b5a1328b36784078eb82801272776c81c501e1e
parkUiery <parkuiery@gmail.com>
2023-06-06 23:09:40 +0900

    클래스의 멤버 메소드

---
+++ MethodTest/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Class1 c1= new Class1();
+        c1.method1();
+
+        c1.add(100,200);
+
+        int result = c1.resultAdd(500,600);
+        System.out.println("리턴 받은 값: " + result);
+    }
+}
