59c439bb3ddc0528bd5ac43b56dc09656af3bd1c
parkUiery <parkuiery@gmail.com>
2023-06-08 16:00:34 +0900

    클래스의 멤버변수

--- VariableTest/src/Class1.java
+++ VariableTest/src/Class1.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Class1 {
+public class Class1 {
+    int a;
+
+    public void method1() {
+        int a = 20;
+        System.out.println("메서드의 지역 변수a : " + a);
+        System.out.println("클래스 멤버 변수a : " + this.a);
+    }
+
+    public void method2() {
+        int a = 30;
+        System.out.println("메소드 2의 지역 변수 a : " + a);
+    }
 }
