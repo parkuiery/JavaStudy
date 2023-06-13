bd6441c3507a0154c879fc88e27b77b2aded505c
parkUiery <parkuiery@gmail.com>
2023-06-13 19:54:06 +0900

    패키지

--- BeakJoon/src/Main.java
+++ BeakJoon/src/Main.java
@@ -5,18 +5,21 @@ public class Main {
 
         int n1 = 0;
         int n2 = 0;
-        int n3 = 0;
 
         Scanner sc = new Scanner(System.in);
 
         n1 = sc.nextInt();
         n2 = sc.nextInt();
-        n3 = sc.nextInt();
-        System.out.println((n1+n2)%n3);
-        System.out.println(((n1%n3)+(n2%n3))%n3);
-        System.out.println((n1*n2)%n3);
-        System.out.println(((n1%n3)*(n2%n3))%n3);
 
+        if(n1>0&&n2>0){
+            System.out.println("1");
+        } else if (n1<0&&n2>0) {
+            System.out.println("2");
+        } else if (n1<0&&n2<0) {
+            System.out.println("3");
+        } else if (n1>0&&n2<0) {
+            System.out.println("4");
+        }
 
     }
 }
